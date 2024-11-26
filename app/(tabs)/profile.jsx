d551632def6b7fed258eba32a9ef5d3f35923fd7
hainam1411 <hainam141199@gmail.com>
2024-11-26 12:19:32 +0700

    css categories

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,20 +1,19 @@
-import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native'
-import React, {useState} from 'react'
-import {SafeAreaView} from "react-native-safe-area-context";
-import {icons} from "../../constants";
-import {Link, useNavigation} from "expo-router";
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { icons } from "../../constants";
+import { Link } from "expo-router";
 import Category from "../../components/category";
 
 const categories = ["Uu dai", "Lich su"];
 const Profile = () => {
     const [selectedCategory, setSelectedCategory] = useState(null);
-    // const navigation =useNavigation();
-    return (<SafeAreaView className=" flex-1 bg-[#FFEFE0]">
 
+    return (
+        <SafeAreaView className="flex-1 bg-[#FFEFE0]">
             <View className="relative flex-row items-center justify-between px-8 pb-10 pt-16">
-                <TouchableOpacity href="/home" className=" w-10 h-10 items-center">
-                    <Image source={icons.leftArrow} className="w-10 h-10" resizeMode='center'/>
-
+                <TouchableOpacity href="/home" className="w-10 h-10 items-center">
+                    <Image source={icons.leftArrow} className="w-10 h-10" resizeMode='center' />
                 </TouchableOpacity>
                 <View>
                     <Text className="font-psemibold text-2xl">Điểm thưởng</Text>
@@ -22,24 +21,34 @@ const Profile = () => {
             </View>
 
             <View className="flex-row items-center bg-white mx-4 rounded-3xl py-1 px-4 shadow-lg mb-4 min-h-[90px]">
-                <Image source={icons.star} className="mr-4"/>
+                <Image source={icons.star} className="mr-4" />
                 <View>
                     <Text className="text-lg text-gray-800">
-                        Số Egg Points hiện tại của bạn </Text>
+                        Số Egg Points hiện tại của bạn
+                    </Text>
                     <Text className="text-orange-400">100</Text>
                 </View>
             </View>
-        <View className="flex-1 items-center">
-            <FlatList data={categories}
-                        renderItem={({item}) => <Category item={item} selectedCategory={selectedCategory}
-                                                          setSelectedCategory={setSelectedCategory}/>}
-                        keyExtractor={(item) => item}
-                        horizontal={true} showsHorizontalScrollIndicator={false}
-            // contentContainerStyle={{justifyContent: 'center', alignItems: 'center', marginTop: 8 }}
-        /></View>
 
+            <View className="items-center">
+                <FlatList
+                    data={categories}
+                    renderItem={({ item }) => (
+                        <Category
+                            item={item}
+                            selectedCategory={selectedCategory}
+                            setSelectedCategory={setSelectedCategory}
+                        />
+                    )}
+                    keyExtractor={(item) => item}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{ justifyContent: 'space-evenly', flexGrow: 1 }}
+                    style={{ width: '100%' }}
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
 
-        </SafeAreaView>)
-}
-export default Profile
-
+export default Profile;
--- components/category.jsx
+++ components/category.jsx
@@ -1,10 +1,10 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 
 const Category = ({ item, selectedCategory, setSelectedCategory }) => {
     return (
-        <TouchableOpacity onPress={() => setSelectedCategory(item)}>
-            <Text className={`text-2xl font-bold px-4 mx-2 py-2 rounded-xl min-h-[62px] justify-center text-center bg-[#F4F4F4] text-[#858587] ${
+        <TouchableOpacity onPress={() => setSelectedCategory(item)} className="flex-1 mx-2">
+            <Text className={`text-2xl font-bold px-4 py-2 rounded-3xl min-h-[62px] bg-[#F4F4F4] text-[#858587] text-center ${
                 selectedCategory == item ? 'bg-[#FFF5F3] text-[#DF7B64]' : ''
             }`}>
                 {item}
