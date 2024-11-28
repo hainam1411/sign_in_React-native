1459721a45bb421b0ffe8185cecc0dc47dacaf3e
hainam1411 <hainam141199@gmail.com>
2024-11-28 14:28:35 +0700

    navigate to home screen

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -7,8 +7,11 @@ import Promo from "../../components/promo";
 import { promos } from "../../constants/promos";
 import { used } from "../../constants/used";
 import History from "../../components/history";
+import { LinearGradient } from "expo-linear-gradient";
+import { useNavigation } from '@react-navigation/native'; // Import useNavigation
 
 const Profile = () => {
+    const navigation = useNavigation(); // Khai báo hook useNavigation
     const [selectedCategory, setSelectedCategory] = useState("Ưu đãi");
 
     const data = {
@@ -17,50 +20,53 @@ const Profile = () => {
     };
 
     return (
-        <ScrollView className="bg-[#FFEFE0]">
-            <View className="relative flex-row items-center justify-between px-8 pb-10 pt-16">
-                <TouchableOpacity href="/home" className="w-10 h-10">
-                    <Image source={icons.leftArrow} className="w-10 h-10" resizeMode="center" />
-                </TouchableOpacity>
-                <Text className="absolute left-1/2 transform -translate-x-1/2 font-psemibold text-2xl">Điểm thưởng</Text>
-            </View>
-
-            <View className="flex-row items-center bg-white mx-4 rounded-3xl py-1 px-4 shadow-lg mb-4 min-h-[90px]">
-                <Image source={icons.star} className="mr-4" />
-                <View>
-                    <Text className="text-lg text-gray-800">Số Egg Points hiện tại của bạn</Text>
-                    <Text className="text-orange-400">100</Text>
+        <ScrollView className="flex-1">
+            <LinearGradient colors={['#FFEFE0', '#FFFFFF']}
+                            style={{ flex: 1 }}>
+                <View className="relative flex-row items-center justify-between px-8 pb-10 pt-16">
+                    <TouchableOpacity onPress={() => navigation.navigate('home')} className="w-10 h-10">
+                        <Image source={icons.leftArrow} className="w-10 h-10" resizeMode="center" />
+                    </TouchableOpacity>
+                    <Text className="absolute left-1/2 transform -translate-x-1/2 font-psemibold text-2xl">Điểm thưởng</Text>
                 </View>
-            </View>
 
-            <View className="flex-row justify-evenly py-3">
-                <TouchableOpacity onPress={() => setSelectedCategory("Ưu đãi")}>
-                    <Category item="Ưu đãi" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => setSelectedCategory("Lịch sử")}>
-                    <Category item="Lịch sử" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-                </TouchableOpacity>
-            </View>
-
-            {selectedCategory === "Ưu đãi" && (
-                <View className="px-4 py-2 flex-row flex-wrap justify-between">
-                    {data["Ưu đãi"].map((promo) => (
-                        <View key={promo.id} className="w-[48%] mb-4">
-                            <Promo title={promo.name} points={promo.point} />
-                        </View>
-                    ))}
+                <View className="flex-row items-center bg-white mx-4 rounded-3xl py-1 px-4 shadow-lg mb-4 min-h-[90px]">
+                    <Image source={icons.star} className="mr-4" />
+                    <View>
+                        <Text className="text-lg text-gray-800">Số Egg Points hiện tại của bạn</Text>
+                        <Text className="text-orange-400">100</Text>
+                    </View>
                 </View>
-            )}
 
-            {selectedCategory === "Lịch sử" && (
-                <View className="px-4 py-2 flex-row flex-wrap justify-between">
-                    {data["Lịch sử"].map((history) => (
-                        <View key={history.id} className="w-[48%] mb-4">
-                            <History title={history.name} points={history.date} />
-                        </View>
-                    ))}
+                <View className="flex-row justify-evenly py-3">
+                    <TouchableOpacity onPress={() => setSelectedCategory("Ưu đãi")}>
+                        <Category item="Ưu đãi" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setSelectedCategory("Lịch sử")}>
+                        <Category item="Lịch sử" selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+                    </TouchableOpacity>
                 </View>
-            )}
+
+                {selectedCategory === "Ưu đãi" && (
+                    <View className="px-4 py-2 flex-row flex-wrap justify-between">
+                        {data["Ưu đãi"].map((promo) => (
+                            <View key={promo.id} className="w-[48%] mb-4">
+                                <Promo title={promo.name} points={promo.point} />
+                            </View>
+                        ))}
+                    </View>
+                )}
+
+                {selectedCategory === "Lịch sử" && (
+                    <View className="px-4 py-2 flex-row flex-wrap justify-between">
+                        {data["Lịch sử"].map((history) => (
+                            <View key={history.id} className="w-[48%] mb-4">
+                                <History title={history.name} points={history.date} />
+                            </View>
+                        ))}
+                    </View>
+                )}
+            </LinearGradient>
         </ScrollView>
     );
 };
